004ee2a7d1c7e4cf2a50a422432de25d40207ea4
Sabith <65082465@users.noreply.github.com>
2023-11-28 15:01:09 +0530

    removed redundant class

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className="sticky top-0 font-medium text-4xl py-3 px-3 bg-[#0005] backdrop-blur backdrop-saturate-[180%] shadow-xl text-slate-100">
+      <div className="font-medium text-4xl py-3 px-3 bg-[#0005] backdrop-blur backdrop-saturate-[180%] shadow-xl text-slate-100">
         <a href="/">ChatBit</a>
       </div>
     </>
