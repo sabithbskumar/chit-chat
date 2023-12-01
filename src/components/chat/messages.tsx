83358db2b023061aed7bfa9939a380cbec4e9d2d
Sabith <65082465@users.noreply.github.com>
2023-12-01 09:59:51 +0530

    updated chat bubble style

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -14,21 +14,29 @@ export function Messages({ messages }: { messages: MessageI[] }) {
       <div className="grow m-2 mr-0 gutter-stable overflow-hidden overflow-y-auto flex gap-1 flex-col-reverse rounded-sm">
         {messages.map((message) => (
           <>
-            <div
-              key={message.id}
-              className={`max-w-fit rounded-md px-6 py-3 bg break-all bg-opacity-60 ${
-                message.uid.toString() == id
-                  ? "self-end bg-blue-500 "
-                  : "bg-green-500"
-              }`}
-            >
+            <div key={message.id} className="flex flex-col group/message">
               {message.uid.toString() != id && (
-                <p className="font-bold underline">
-                  {message.name}
-                  {"\n"}
+                <p className="text-sm font-bold">{message.name}</p>
+              )}
+              <div
+                key={message.id}
+                className={`max-w-fit rounded-lg px-6 py-3 break-all bg-gradient-to-tr bg-opacity-60 ${
+                  message.uid.toString() == id
+                    ? "ml-4 self-end from-cyan-500 to-blue-500"
+                    : "mr-4 ml-2 from-green-500 to-emerald-500"
+                }`}
+              >
+                <p>{message.message}</p>
+              </div>
+              {message.uid.toString() != id ? (
+                <p className="opacity-0 group-hover/message:opacity-100 transition-opacity hover:block pl-3 text-xs">
+                  {new Date(message.id).toLocaleTimeString()}
+                </p>
+              ) : (
+                <p className="opacity-0 group-hover/message:opacity-100 transition-opacity text-right text-xs">
+                  {new Date(message.id).toLocaleTimeString()}
                 </p>
               )}
-              <p>{message.message}</p>
             </div>
           </>
         ))}
