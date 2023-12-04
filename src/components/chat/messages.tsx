b8084d9c7dc491c119b539ac0188a3bcb82f72cf
Sabith <65082465@users.noreply.github.com>
2023-12-04 10:02:12 +0530

    message date style change

--- src/components/chat/messages.tsx
+++ src/components/chat/messages.tsx
@@ -12,29 +12,21 @@ export function Messages({ messages }: { messages: MessageI[] }) {
   return (
     <div className="grow m-2 mr-0 gutter-stable overflow-hidden overflow-y-auto flex gap-1 flex-col-reverse rounded-sm">
       {messages.map((message) => (
-        <div key={message.id} className="flex flex-col group/message">
+        <div key={message.id} className="flex flex-col relative">
           {message.uid.toString() != id && (
             <p className="text-sm font-bold">{message.name}</p>
           )}
           <div
             key={message.id}
-            className={`max-w-fit rounded-lg px-6 py-3 break-all bg-gradient-to-tr ${
+            className={`max-w-fit rounded-lg px-6 py-3 mb-3 break-all bg-gradient-to-tr after:content-[attr(data-time)] after:text-xs after:block after:absolute after:-bottom-0.5 after:left-0 after:right-0 after:opacity-0 hover:after:opacity-100 after:transition-opacity ${
               message.uid.toString() == id
-                ? "ml-4 self-end from-cyan-500 to-blue-500"
-                : "mr-4 ml-2 from-green-500 to-emerald-500"
+                ? "ml-4 self-end from-cyan-500 to-blue-500 after:text-right"
+                : "mr-4 ml-2 from-green-500 to-emerald-500 after:ml-3"
             }`}
+            data-time={new Date(message.id).toLocaleTimeString()}
           >
             <p>{message.message}</p>
           </div>
-          {message.uid.toString() != id ? (
-            <p className="opacity-0 group-hover/message:opacity-100 transition-opacity hover:block pl-3 text-xs">
-              {new Date(message.id).toLocaleTimeString()}
-            </p>
-          ) : (
-            <p className="opacity-0 group-hover/message:opacity-100 transition-opacity text-right text-xs">
-              {new Date(message.id).toLocaleTimeString()}
-            </p>
-          )}
         </div>
       ))}
     </div>
